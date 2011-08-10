391043684746a01fe9b06ea512872bfe9ecc2aec
mattiuzzi5114 (DI ) <matteo.mattiuzzi@boku.ac.at>
2011-08-10 19:25:50 +0000

    first test!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/getmodis@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/getMODIS/R/getXML.R
@@ -0,0 +1,73 @@
+getXML <-
+function(LocalPathToHdf="",HdfName="", wait=2,comments=FALSE){
+
+###################
+if (LocalPathToHdf!=""){
+LocalPathToHdf <- path.expand(LocalPathToHdf)
+} else {
+LocalPathToHdf <- "." # getwd()  # probably not needed because of default
+}
+
+if(HdfName!="") {
+HdfName <- unlist(HdfName)
+avFiles <- list()
+for ( i in seq(length(HdfName))){
+if (file.exists(HdfName[i])) {
+avFiles[[i]] <- HdfName[i] # if exists than HdfName is a path+File+itexists 
+} else {
+avFiles[[i]] <- list.files(LocalPathToHdf,pattern=HdfName[i],recursive=TRUE,full.names=TRUE)
+avFiles[[i]] <- grep(avFiles[[i]], pattern=".hdf$",value=TRUE) # removes xml files from list 
+}
+}
+avFiles <- unlist(avFiles)
+} else {
+avFiles <- list.files(LocalPathToHdf,pattern=".hdf$",recursive=TRUE,full.names=TRUE) # all hdf under the home dir...
+}
+
+if(length(avFiles)==0) {cat("No files to download\n")} else {
+
+    for (u in seq(along=avFiles)){
+
+	if ( !file.exists(paste(avFiles[u],".xml",sep="")) || # if xml-file doesn't exists 
+
+	 	if ( .Platform$OS.type == "unix") {as.numeric(system(paste("stat -c %s ",avFiles[u],".xml",sep=""), intern=TRUE)) < 2000}else{FALSE} # tested on Ubuntu 11.04
+# 		if ( .Platform$OS.type == "windows") {as.numeric(system(paste("for %I in (",avFiles[u],") do @echo %~zI",sep=""),intern=TRUE)) < 2000} # sould work with win2000 and later...but not tested! (http://stackoverflow.com/questions/483864/windows-command-for-file-size-only)
+# 		if (!.Platform$OS.type %in% c("unix","windows")) {FALSE} # if not unix or windows, skip this test...(for now)
+	# if file exists but smaller than 2000 B...  so probably brocken download
+	  ){
+
+	name <- strsplit(avFiles[u],"/")[[1]] # separate name from path
+	name <- name[length(name)] # select filename
+
+	secName  <- strsplit(name,"\\.")[[1]] # decompose filename
+
+	PF <- substr(secName[1],1,3)
+	if(PF=="MOD"){PF <- "MOLT"}else {PF <- "MOLA"}
+
+	date <- substr(secName[2],2,8)
+	date <- format(as.Date(as.numeric(substr(date,5,7))-1,origin=paste(substr(date,1,4),"-01-01",sep="")),"%Y.%m.%d")
+
+	version <- secName[4]
+
+	require(RCurl) # is it good here?
+	download.file(
+		paste("ftp://e4ftl01u.ecs.nasa.gov/", PF,"/",secName[1],".",version,"/",date,"/",name,".xml",sep=""),
+		destfile=paste(avFiles[u],".xml",sep=""),
+		mode='wb', method='wget', quiet=F, cacheOK=FALSE)
+
+
+		if (comments){
+			cat(paste("downloaded file: ", name,".xml\n\n",sep=""))
+			flush.console()
+		}
+
+		if (wait!=0){
+			require(audio) # for wait() # is it good here?
+			wait(wait) # waiting seams to decrease the chanse of ftp collapse
+			}
+		}
+	}
+}
+
+} # end getMODIS::.getXML
+
