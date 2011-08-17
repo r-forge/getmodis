eca50e6a16933f48b97df56c29eeebcffa6471d7
mattiuzzi5114 (DI ) <matteo.mattiuzzi@boku.ac.at>
2011-08-17 08:27:01 +0000

    hopefilly corrected lat/lon min/max confusions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/getmodis@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/getMODIS/R/getTILE.R
+++ pkg/getMODIS/R/getTILE.R
@@ -5,59 +5,7 @@
 
 getTILE <- function(tileH,tileV,extent) {
 
-tiles <- list()
-
-
-# extent class "list"
-if (!missing(extent) && class(extent) == "list"){
-
-data("tiletable") # this file solves the dependency from LDOPE (for this task!)
-  # Austria <- list(lat_min=46.12,lat_max=49.3,lon_max=17.47,lon_min=9.2) # h18v04 and h19v04
-
-  minTile <- subset(tiletable,
-                  (tiletable$lon_min <= extent$lon_min & tiletable$lon_max >= extent$lon_min) &
-                  (tiletable$lat_min <= extent$lat_max & tiletable$lat_max >= extent$lat_max)
-         ,select=c(iv,ih))
-  minTile <- c(min(minTile$iv),min(minTile$ih))
-    
-  maxTile <-  subset(tiletable,
-                  (tiletable$lon_min <= extent$lon_max & tiletable$lon_max >= extent$lon_max) &
-                  (tiletable$lat_min <= extent$lat_min & tiletable$lat_max >= extent$lat_min)
-         ,select=c(iv,ih))
-  maxTile <- c(max(maxTile$iv),max(maxTile$ih))
-
-  tileV <- minTile[1]:maxTile[1]
-  tileH <- minTile[2]:maxTile[2]
-}
-
-# extent class "raster* object (extent)"
-if (!missing(extent) && class(extent) %in% c("Extent","RasterLayer","RasterStack","RasterBrick") ){
-
-require(raster) # should allready be loaded... but anyway
-
-	if (class(extent) != "Extent"){  
-		extent <- extent(extent)# checking if lat/lon !!??
-	} 
-
-data("tiletable")
-
-  minTile <- subset(tiletable,
-                  (tiletable$lon_min <= extent@xmin & tiletable$lon_max >= extent@xmin) &
-                  (tiletable$lat_min <= extent@ymax & tiletable$lat_max >= extent@ymax)
-         ,select=c(iv,ih))
-  minTile <- c(min(minTile$iv),min(minTile$ih))
-  
-  maxTile <-  subset(tiletable,
-                  (tiletable$lon_min <= extent@xmax & tiletable$lon_max >= extent@xmax) &
-                  (tiletable$lat_min <= extent@ymin & tiletable$lat_max >= extent@ymin)
-         ,select=c(iv,ih))
-  maxTile <- c(max(maxTile$iv),max(maxTile$ih))
-
-  tileV <- minTile[1]:maxTile[1]
-  tileH <- minTile[2]:maxTile[2]
-  
-}
-###########################
+###########################################
 # from maps
 
 #if (!missing(extent) && class(map)){
@@ -71,10 +19,44 @@ data("tiletable")
 # is <- grep(ex$names,pattern="Austria",ignore.case=T)
 
 #}
+############################################
+# extent class "raster* object (extent)"
+if (!missing(extent) && class(extent) %in% c("Extent","RasterLayer","RasterStack","RasterBrick") ){
+
+require(raster) # should allready be loaded... but anyway
+
+	if (class(extent) != "Extent"){  
+		extent <- extent(extent)# checking if lat/lon !!??
+	} 
+
+extent <- list(lat_min=extent@ymin,lat_max=extent@ymax,lon_min=extent@xmin,lon_max=extent@xmax)
+}
 
 ####################################
-# get the results
+# extent class "list"
+if (!missing(extent) && class(extent) == "list"){# missing refers to function var, can be result of if raster...
 
+data("tiletable")
+
+  minTile <- subset(tiletable,
+                  (tiletable$lon_min <= extent$lon_min & tiletable$lon_max >= extent$lon_min) &
+                  (tiletable$lat_min <= extent$lat_min & tiletable$lat_max >= extent$lat_min)
+         ,select=c(iv,ih))
+  minTile <- c(min(minTile$iv),min(minTile$ih))
+    
+  maxTile <-  subset(tiletable,
+                  (tiletable$lon_min <= extent$lon_max & tiletable$lon_max >= extent$lon_max) &
+                  (tiletable$lat_min <= extent$lat_max & tiletable$lat_max >= extent$lat_max)
+         ,select=c(iv,ih))
+  maxTile <- c(max(maxTile$iv),max(maxTile$ih))
+
+  tileV <- minTile[1]:maxTile[1]
+  tileH <- minTile[2]:maxTile[2]
+}
+
+###################################
+# get the results
+tiles <- list()
 tileH <- as.vector(tileH)
 	if (tileH < 0 || tileH > 36) {stop("'tileH' number(s) must be between 0 and 35")}
 tileV <- as.vector(tileV)
@@ -82,7 +64,8 @@ tileV <- as.vector(tileV)
 for (i in seq(along=tileH)){
 	tiles[[i]] <- paste("h",sprintf("%02d",tileH[i]),"v",sprintf("%02d",tileV),sep="")	
 }
+result <- list(tile=unlist(tiles),tileH=tileH,tileV=tileV)
 
-return(unlist(tiles))
+return(result)
 }
 
