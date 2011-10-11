7b278a2ae351f708724ebb596e98caad1135b5c5
mattiuzzi5114 (DI ) <matteo.mattiuzzi@boku.ac.at>
2011-10-11 14:47:01 +0000

    bug in updating information removed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/getmodis@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/getMODIS/R/getSTRUC.R
+++ pkg/getMODIS/R/getSTRUC.R
@@ -91,12 +91,12 @@ for (i in 1:length(product$PF2)){
 	if (getIT) { # the return is 'FtpDayDirs' of the requested product
 
 		ftp <- paste("ftp://e4ftl01.cr.usgs.gov/",product$PF1[i],"/", productNameFull,"/",sep="")
-#		ftp <- paste("ftp://e4ftl01u.ecs.nasa.gov/",product$PF1[i],"/", productNameFull,"/",sep="")
+#		ftp <- paste("ftp://e4ftl01u.ecs.nasa.gov/",product$PF1[i],"/", productNameFull,"/",sep="") # old ftp
 		cat("Getting structure for:", ftp,"\n")	
 		require(RCurl)
 		FtpDayDirs  <- getURL(ftp)
 	
-			if (wait > 0 & i != length(product$PF2)) {
+			if (wait > 0 && i != length(product$PF2)) {
 					require(audio)
 					wait(wait)
 					}
@@ -104,10 +104,26 @@ for (i in 1:length(product$PF2)){
 		FtpDayDirs  <- unlist(strsplit(FtpDayDirs[[1]], if(.Platform$OS.type=="unix"){"\n"}else{"\r\n"})) # Is this enought? Mac? Solaris?....
 		FtpDayDirs  <- FtpDayDirs[substr(FtpDayDirs, 1, 1)=='d'] 
 		FtpDayDirs  <- unlist(lapply(strsplit(FtpDayDirs, " "), function(x){x[length(x)]}))
+		
+			if (!createNew) {	
+				FtpDayDirs <- matrix(FtpDayDirs)
+				mtr <- matrix(NA,ncol=ncol(ftpdirs),nrow=max(length(FtpDayDirs),dim(ftpdirs)[1]))
+				colnames(mtr) <- colnames(ftpdirs)	
+			
+				if (ncol(ftpdirs)!=0){
+					for(j in 1:ncol(ftpdirs)){
+						mtr[,j] <- replace(mtr[,j], 1:nrow(ftpdirs),ftpdirs[,j])
+					}
+				}
+			
+				mtr[,productNameFull] <- replace(mtr[,productNameFull], 1:length(FtpDayDirs),FtpDayDirs) 
+				ftpdirs <- mtr
+			}
+		
 	}
 
 
-	if (createNew) { # write/update to ftpdir.txt
+	if (createNew) { # creates the new ftpdir.txt
 		FtpDayDirs <- matrix(FtpDayDirs)
 		mtr <- matrix(NA,ncol=ncol(ftpdirs)+1,nrow=max(length(FtpDayDirs),dim(ftpdirs)[1]))
 		colnames(mtr) <- if(ncol(ftpdirs)>0){c(colnames(ftpdirs),productNameFull)} else {productNameFull}	
@@ -120,7 +136,7 @@ for (i in 1:length(product$PF2)){
 			
 		mtr[,ncol(mtr)] <- replace(mtr[,ncol(mtr)], 1:length(FtpDayDirs),FtpDayDirs) 
 		ftpdirs <- mtr
-	}
+	} 
 
 }
 
